4b2e4aa42776809b7d1164ea68720ab43ad2811d
Mohammad <moh16mah@gmail.com>
2018-05-17 18:08:51 +0430

    add 2 lpf and mpf methods to getNext function

--- src/sudoku/main.java
+++ src/sudoku/main.java
@@ -1,14 +1,15 @@
 package sudoku;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class main {
-    final String METHOD_NAME="NAIVE";
+    int method=0;
+    final String[] METHOD_NAMES={"naive","lpf","mpf"};
     Stack<State> fringe=new Stack<>();
     long expandedStatesCount=0L,backtracks=0L,sum=0L;
     long startTime;
@@ -23,38 +24,44 @@ public class main {
         for(int i=0;i<81;i++){
             bufferOptions.add(new Tile((ArrayList<Integer>) variableDomain.clone(),i,0));
         }
-
         List<String> initialBoards= Files.readAllLines(Paths.get("euler.txt"));
-        for(String board:initialBoards) {
-            State initialState = new State(bufferOptions);
-            int index=-1;
-            for(char num:board.toCharArray()){
-                index++;
-                if(num=='.')continue;
-                initialState=applyOption(initialState,index,num-'0');
+        for(method=0;method<METHOD_NAMES.length;method++) {
+            int ind=0;
+            for (String board : initialBoards) {
+                ind++;
+                State initialState = new State(bufferOptions);
+                int index = -1;
+                for (char num : board.toCharArray()) {
+                    index++;
+                    if (num == '.') continue;
+                    initialState = applyOption(initialState, index, num - '0');
+                }
+                startTime = System.currentTimeMillis();
+                expandedStatesCount = 0L;
+                backtracks = 0;
+//                System.out.println(initialState);
+                fringe.add(initialState);
+                while (!fringe.isEmpty())
+                    expand();
+
             }
-            startTime = System.currentTimeMillis();
-            expandedStatesCount=0L;
-            backtracks=0;
-            System.out.println(initialState);
-            fringe.add(initialState);
-            while (!fringe.isEmpty())
-                expand();
+            System.out.println(METHOD_NAMES[method]+" has been finished");
+            Files.write(Paths.get(METHOD_NAMES[method] + "_data.txt"), details, StandardOpenOption.CREATE);
+            details.clear();
         }
-        Files.write(Paths.get(METHOD_NAME+"_data.txt"),details, StandardOpenOption.CREATE);
     }
     void expand(){
         State finalState=fringe.peek();
         expandedStatesCount++;
-        int index= getNextNaive(finalState);
+        int index= getNext(finalState,method);
         if(index==82){
             long elapsedTime=(System.currentTimeMillis()-startTime);
             sum+=finalState.tiles.get(0).value*100+finalState.tiles.get(1).value*10+finalState.tiles.get(2).value;
-            System.out.println("solved!");
-            System.out.println(finalState);
-            System.out.println("expanded states :"+expandedStatesCount);
-            System.out.println("backtracks :"+backtracks);
-            System.out.println("solve time in ms: "+elapsedTime);
+//            System.out.println("solved!");
+//            System.out.println(finalState);
+//            System.out.println("expanded states :"+expandedStatesCount);
+//            System.out.println("backtracks :"+backtracks);
+//            System.out.println("solve time in ms: "+elapsedTime);
             details.add(expandedStatesCount+","+backtracks+","+elapsedTime);
             fringe.removeAllElements();
             return;
@@ -85,12 +92,18 @@ public class main {
         return newState;
     }
 
-    int getNextNaive(State state){
-        for(int i=0;i<81;i++)
-            if(state.tiles.get(i).value==0) return i;
-        return 82;
+    int getNext(State state,int method){
+        if(method==0){
+            for(int i=0;i<81;i++)
+                if(state.tiles.get(i).value==0) return i;
+            return 82;
+        }
+        List<Tile> emptyTiles=state.tiles.stream().filter(tile-> tile.value==0).collect(Collectors.toList());
+        if(emptyTiles.isEmpty())return 82;
+        Collections.sort(emptyTiles, (Tile o1, Tile o2)-> (2-method)*Integer.compare(o1.options.size(),o2.options.size())+(method-1)*Integer.compare(o1.options.size(),o2.options.size()));
+        return emptyTiles.get(0).tileIndex;
     }
-    
+
 
     public static void main(String[] args) throws IOException {
         new main();
