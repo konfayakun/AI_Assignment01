172d08b00dda6d448eaeaf5ffafcaa1f6725aa6f
Mohammad <moh16mah@gmail.com>
2018-05-19 02:43:52 +0430

    correct misunderstood methods in backtrack ordering. MRV and MRV+LCV methods added. Ordering phase completed.

--- src/sudoku/main.java
+++ src/sudoku/main.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 
 public class main {
     int method=0;
-    final String[] METHOD_NAMES={"naive","lpf","mpf"};
+    final String[] METHOD_NAMES={"naive","mrv","mrv+lcv"};
     Stack<State> fringe=new Stack<>();
     long expandedStatesCount=0L,backtracks=0L,sum=0L;
     long startTime;
@@ -24,7 +24,7 @@ public class main {
         for(int i=0;i<81;i++){
             bufferOptions.add(new Tile((ArrayList<Integer>) variableDomain.clone(),i,0));
         }
-        List<String> initialBoards= Files.readAllLines(Paths.get("euler.txt"));
+        List<String> initialBoards= Files.readAllLines(Paths.get("magictour.txt"));
         for(method=0;method<METHOD_NAMES.length;method++) {
             int ind=0;
             for (String board : initialBoards) {
@@ -49,6 +49,7 @@ public class main {
             Files.write(Paths.get(METHOD_NAMES[method] + "_data.txt"), details, StandardOpenOption.CREATE);
             details.clear();
         }
+        System.out.println(sum);
     }
     void expand(){
         State finalState=fringe.peek();
@@ -72,7 +73,34 @@ public class main {
             return;
         }
         Tile tile=finalState.tiles.get(index);
-        int currentStep=tile.options.remove(0); // remove current step form top stack state
+        int currentStep;
+        if(method==3) { //lcv
+            int bestValue = -1, minConstraining = Integer.MAX_VALUE;
+            for (int value : tile.options) {
+                int bufferConstraining = 0;
+                int i = index / 9, j = index % 9;
+                for (int offset = 0; offset < 9; offset++) {
+                    if (finalState.tiles.get((offset) * 9 + j).options.contains(Integer.valueOf(value)))
+                        bufferConstraining++; //row
+                    if (finalState.tiles.get(i * 9 + offset).options.contains(Integer.valueOf(value)))
+                        bufferConstraining++; //col
+                }
+                for (int xoffset = 0; xoffset < 3; xoffset++) {
+                    for (int yoffset = 0; yoffset < 3; yoffset++) {
+                        if (finalState.tiles.get(((i / 3) * 3 + xoffset) * 9 + (j / 3) * 3 + yoffset).options.remove(Integer.valueOf(value)))
+                            bufferConstraining++; //block
+                    }
+                }
+                if (minConstraining > bufferConstraining) {
+                    minConstraining = bufferConstraining;
+                    bestValue = value;
+                }
+            }
+            tile.options.remove(Integer.valueOf(bestValue)); // remove current step form top stack state
+            currentStep=bestValue;
+        }
+        else
+            currentStep=tile.options.remove(0); // remove current step form top stack state
         fringe.push(applyOption(finalState,tile.tileIndex,currentStep));
         expand();
     }
@@ -93,15 +121,14 @@ public class main {
     }
 
     int getNext(State state,int method){
-        if(method==0){
-            for(int i=0;i<81;i++)
-                if(state.tiles.get(i).value==0) return i;
-            return 82;
-        }
-        List<Tile> emptyTiles=state.tiles.stream().filter(tile-> tile.value==0).collect(Collectors.toList());
-        if(emptyTiles.isEmpty())return 82;
-        Collections.sort(emptyTiles, (Tile o1, Tile o2)-> (2-method)*Integer.compare(o1.options.size(),o2.options.size())+(method-1)*Integer.compare(o1.options.size(),o2.options.size()));
-        return emptyTiles.get(0).tileIndex;
+        List<Tile> emptyTiles=state.tiles.stream().filter(tile-> tile.value==0).collect(Collectors.toList()); // get empty tiles
+        if(emptyTiles.isEmpty())return 82;                                                                   // solved if all tiles are not empty
+        Comparator<Tile> comparator=null;
+        if(method==0)
+            comparator=Comparator.comparingInt(tile->tile.tileIndex);
+        else
+            comparator=Comparator.comparingInt(tile->tile.options.size());
+        return Collections.min(emptyTiles,comparator).tileIndex;
     }
 
 
